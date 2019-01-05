ecef2f89bec792bab3589ba154aba512185532f5
apple <apple@apples-MacBook-Pro.local>
2019-01-05 15:31:09 +0530

    add inital Commit

---
+++ Oauth2SpringBoot/src/main/java/poc/oauth/emc/wsje/Oauth2SpringBootApplication.java
@@ -0,0 +1,14 @@
+package poc.oauth.emc.wsje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SpringBootApplication.class, args);
+	}
+
+}
+
---
+++ Oauth2SpringBoot/src/test/java/poc/oauth/emc/wsje/Oauth2SpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package poc.oauth.emc.wsje;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2SpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
